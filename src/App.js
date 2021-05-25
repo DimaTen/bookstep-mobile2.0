a235f1b5a2278f1bbf8149e8a405b0891ccc4f20
magdabq <bergqv.m@gmail.com>
2021-05-25 14:28:53 +0200

    ändring app.js före checkout joels branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import './styles.css';
-import React, { useState } from 'react';
-import Sidemenu from './components/Sidemenu';
-import Popup from './components/Popup';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { AuthProvider } from './contexts/AuthContext';
-import MainContent from './components/MainContent';
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles.css";
+import React, { useState } from "react";
+import Sidemenu from "./components/Sidemenu";
+import Popup from "./components/Popup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { AuthProvider } from "./contexts/AuthContext";
+import MainContent from "./components/MainContent";
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,14 +14,8 @@ const App = () => {
     <Router>
       <AuthProvider>
         <div className="container-fluid">
-          <MediaQuery query="(max-device-width: 1024px)">
-            <Header isOpen={isOpen} setTrigger={setIsOpen} />
-            <section className="hider"></section>
-          </MediaQuery>
-          <MediaQuery query="(min-device-width: 1024px)">
-            <HeaderDesktop/>
-           
-          </MediaQuery>
+          <Header isOpen={isOpen} setTrigger={setIsOpen} />
+          <section className="hider"></section>
           <MainContent setTrigger={setIsOpen} />
           <Popup trigger={isOpen}>
             <Sidemenu setTrigger={setIsOpen} />
