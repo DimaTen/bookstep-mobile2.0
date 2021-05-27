b3afe896e96759b4e28ab800566014c48bbcdf77
DimaTen <73692282+DimaTen@users.noreply.github.com>
2021-05-27 20:25:19 +0200

    playlist is done

--- src/components/Player/components/playlist/Playlist.js
+++ src/components/Player/components/playlist/Playlist.js
@@ -1,23 +1,26 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext,  useRef, useState } from 'react';
+
 import playerContext from '../../context/playerContext';
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import 'swiper/swiper-bundle.css';
 
 
 // Import Swiper styles
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
 
 import "../../main.css";
 
 
 // import Swiper core and required modules
 import SwiperCore, {
-  Pagination
+  Pagination,Navigation,Virtual
 } from 'swiper/core';
 
 // install Swiper modules
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination,Navigation,Virtual]);
 
 function Playlist() {
   const { SetCurrent, currentSong, songs } = useContext(playerContext);
@@ -32,45 +35,80 @@ function Playlist() {
     }
   };
 
+  const virtual = {
+    "slides": (function () {
+            var slides = [];
+            for (var i = 0; i < 600; i += 1) {
+              slides.push('Slide ' + (i + 1));
+            }
+            return slides;
+          })()
+  }
+
+  const swiperRef = useRef(null);
+
+  let appendNumber = 600;
+  let prependNumber = 1;
+
+  const prepend = () => {
+    swiperRef.current.swiper.virtual.prependSlide([
+      'Slide ' + (--prependNumber),
+      'Slide ' + (--prependNumber)
+    ]);
+  }
+
+  const append = () => {
+    swiperRef.current.swiper.virtual.appendSlide('Slide ' + (++appendNumber));
+  }
+
+  const slideTo = (index) => {
+    swiperRef.current.slideTo(index - 1, 0);
+  }
+
+
+
+
   return (
 
 
     <div class="Playlist">
-
-      <div class="playlist-left" id="playlist-scroll">
-        <img src="./icons/Sidearrow-left.png"></img>
+      <div className="swiper-container">
+        <div class="playlist-middle">
+        <Swiper slidesPerView={6.5}  freeMode={true}hashNavigation={{
+          "watchState": true
+        }} p navigation={true} className="mySwiper">
+          <div className="swiper-slides">
+          <ul className="loi">
+              {songs.map((song, i) => (
+              <SwiperSlide src>
+                <li
+                  className={
+                    'songContainer ' + (currentSong === i ? 'selected' : '')
+                  }
+                  key={i}
+                  onClick={() => {
+                    checkSteps(i);
+                  }}
+                >
+                  <div id="chapter-icon">
+                    <span className="song">{song[2]}</span>
+                  </div>
+                </li>
+              </SwiperSlide>
+              ))} 
+            </ul>
+          </div>
+          
+        </Swiper>
+        </div>
       </div>
 
-      <div class="playlist-middle">
-      <Swiper slidesPerView={15} spaceBetween={10} freeMode={true} pagination={{
-        "clickable": true
-      }} className="mySwiper">
-         <ul className="loi">
-            {songs.map((song, i) => (
-            <SwiperSlide src>
-              <li
-                className={
-                  'songContainer ' + (currentSong === i ? 'selected' : '')
-                }
-                key={i}
-                onClick={() => {
-                  checkSteps(i);
-                }}
-              >
-                <div id="chapter-icon">
-                  <span className="song">{song[2]}</span>
-                </div>
-              </li>
-            </SwiperSlide>
-            ))} 
-          </ul>
-      </Swiper>
+      <div class="swiper-button-prev-unique">
+      
       </div>
-
-      <div class="playlist-right" id="playlist-scroll">
-        <img src="./icons/Sidearrow-right.png"></img>
-      </div>
+      <div class="swiper-button-next-unique"></div>
     </div>
+
   );
 }
 
